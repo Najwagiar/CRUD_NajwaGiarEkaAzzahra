76675101932686ad19cd0be074601e66142a017d
awa <najwagiar@gmail.com>
2026-06-17 10:18:10 +0700

    push

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,10 +1,11 @@
+import { authenticat } from "../middlewares/authMiddleware.js";
 import express from "express";
 import { getCategories, createCategory, showCategory, updateCategory,deleteCategory } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.get("/", getCategories);
-router.post("/",createCategory);
+router.post("/", authenticat, createCategory);
 router.get("/:id", showCategory);
 router.put("/:id", updateCategory);
 router.delete("/:id", deleteCategory);
